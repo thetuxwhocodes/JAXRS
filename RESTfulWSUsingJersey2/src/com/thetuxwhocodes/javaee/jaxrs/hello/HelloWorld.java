e618ff0a5dcf9a00d293d473fbea232e5224175e
thetuxwhocodes <invincibletux@gmail.com>
2014-07-30 00:20:56 +0530

    First version of REST project using Jersey2 Not using Maven, hence checking in all jars.

---
+++ RESTfulWSUsingJersey2/src/com/thetuxwhocodes/javaee/jaxrs/hello/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.thetuxwhocodes.javaee.jaxrs.hello;
+ 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+ 
+
+@Path("hello")
+public class HelloWorld {
+ 
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String getIt() {
+        return "Hello World! Have some REST served through Jersey 2.";
+    }
+}
